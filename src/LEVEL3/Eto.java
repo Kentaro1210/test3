41dd256e6127363d277aef26d09349e515ea4277
Kentaro1210 <gorgonzola1210@gmail.com>
2019-12-11 14:52:27 +0900

    LEVEL4 修正版

--- src/LEVEL3/Eto.java
+++ src/LEVEL3/Eto.java
@@ -1,18 +1,33 @@
 package LEVEL3;
+import java.util.Scanner;
 
 public class Eto {
 
     public static void main(String[] args) {
 	
-	//seinenを引数にしてgetJikkanを呼び出し、結果を文字列型で受け取る
-	//seinenを引数にしてgetJunishiを呼び出し、結果を文字列型で受け取る
-        int seinen =2016;
-		    System.out.println(seinen+"年の干支は"+getJikkan(seinen)+getJunishi(seinen)+"です。");
-	}
+	    //seinenを引数にしてgetJikkanを呼び出し、結果を文字列型で受け取る
+	    //seinenを引数にしてgetJunishiを呼び出し、結果を文字列型で受け取る
+    	
+        //西暦をユーザーが入力出来るよう修正しましょう
+    	System.out.println("西暦を入力してください");
+    	
+        Scanner scanner = new Scanner(System.in);
+        int seinen = scanner.nextInt();
+        
+        scanner.close();
+        
+        if (seinen <0) {
+        
+        	//入力された値がマイナスの値の場合、「0以上の値を入力してください」と返すように修正してください	
+            System.out.println("0以上の値を入力してください");
+        } else {
+		    System.out.println(seinen + "年の干支は" + getJikkan(seinen) + getJunishi(seinen)+ "です。"); 
+	    }
+    }
 	
 	//十干をenum型で定義する
     private enum jikkan{
-        庚,辛,壬,癸,甲,乙,丙,丁,戊,己
+        庚, 辛, 壬, 癸, 甲, 乙, 丙, 丁, 戊, 己
     }
 
 	//yearをもとに十干を算出して文字列の形でreturnする
@@ -30,7 +45,7 @@ public class Eto {
 	
 	//十二支をenum型で定義する
 	private enum junishi{
-	    申,酉,戌,亥,子,丑,寅,卯,辰,巳,午,未
+	    申, 酉, 戌, 亥, 子, 丑, 寅, 卯, 辰, 巳, 午, 未
 	}
 
 	//yearをもとに十二支を計算して文字列の形でreturnする
